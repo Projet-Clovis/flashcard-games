cdaa1ee741a03ae1c8c76b5e87cd54da494e38ee
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-29 01:43:46 +0200

    test(quiz): add select answer test

--- src/quiz/components/Quiz.test.tsx
+++ src/quiz/components/Quiz.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import { Quiz } from "./Quiz";
 import "@testing-library/jest-dom";
@@ -14,5 +15,17 @@ describe("Quiz", () => {
         expect(screen.getByText(/a1/i)).toBeInTheDocument();
         expect(screen.getByText(/a2/i)).toBeInTheDocument();
         expect(screen.getByText(/a3/i)).toBeInTheDocument();
+    });
+
+    it("selects an answer", async () => {
+        const user = userEvent.setup({});
+        render(<Quiz questions={questionsTest} />);
+
+        const answers = screen.getAllByRole("button");
+        await user.click(answers[0]);
+
+        expect(answers[0]).toHaveClass("bg-green-100");
+        expect(answers[1]).toHaveClass("bg-gray-100");
+        expect(answers[2]).toHaveClass("bg-gray-100");
     });
 });
