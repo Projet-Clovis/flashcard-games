8ee23e7d215ca5e06b78f9abc2ad693797dfc248
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-30 01:12:47 +0200

    refactor: refactor test data

--- src/shared/data/flashcards-content.ts
+++ src/shared/data/flashcards-content.ts
@@ -1,41 +1,46 @@
 import type { Question } from "../types/flashcardTypes.ts";
 
+export const q_spain: Question = {
+    id: 1,
+    question: "Quelle est la capitale de l'Espagne ?",
+    options: ["Madrid", "Barcelone", "Séville", "Valence"],
+    correctAnswer: "Madrid",
+};
+
+export const q_ocean: Question = {
+    id: 2,
+    question: "Quel est le plus grand océan ?",
+    options: ["Atlantique", "Indien", "Arctique", "Pacifique"],
+    correctAnswer: "Pacifique",
+};
+
+export const q_book: Question = {
+    id: 3,
+    question: "Qui a écrit 'Les Misérables' ?",
+    options: ["Victor Hugo", "Emile Zola", "Gustave Flaubert", "Marcel Proust"],
+    correctAnswer: "Victor Hugo",
+};
+
+export const q_yen: Question = {
+    id: 4,
+    question: "Quelle est la monnaie du Japon ?",
+    options: ["Yen", "Won", "Rupee", "Baht"],
+    correctAnswer: "Yen",
+};
+
+export const q_oxygen: Question = {
+    id: 5,
+    question: "Quel est l'élément chimique avec le symbole 'O' ?",
+    options: ["Oxygène", "Or", "Ozone", "Osmium"],
+    correctAnswer: "Oxygène",
+};
+
 export const questions: Question[] = [
-    {
-        id: 1,
-        question: "Quelle est la capitale de l'Espagne ?",
-        options: ["Madrid", "Barcelone", "Séville", "Valence"],
-        correctAnswer: "Madrid",
-    },
-    {
-        id: 2,
-        question: "Quel est le plus grand océan ?",
-        options: ["Atlantique", "Indien", "Arctique", "Pacifique"],
-        correctAnswer: "Pacifique",
-    },
-    {
-        id: 3,
-        question: "Qui a écrit 'Les Misérables' ?",
-        options: [
-            "Victor Hugo",
-            "Emile Zola",
-            "Gustave Flaubert",
-            "Marcel Proust",
-        ],
-        correctAnswer: "Victor Hugo",
-    },
-    {
-        id: 4,
-        question: "Quelle est la monnaie du Japon ?",
-        options: ["Yen", "Won", "Rupee", "Baht"],
-        correctAnswer: "Yen",
-    },
-    {
-        id: 5,
-        question: "Quel est l'élément chimique avec le symbole 'O' ?",
-        options: ["Oxygène", "Or", "Ozone", "Osmium"],
-        correctAnswer: "Oxygène",
-    },
+    q_spain,
+    q_ocean,
+    q_book,
+    q_yen,
+    q_oxygen,
 ];
 
 export const questionsTest: Question[] = [
