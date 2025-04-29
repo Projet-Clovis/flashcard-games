ad83db8a0b04757c911d812747c67b1c9b63ee99
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-29 22:34:46 +0200

    chore: code ready to be packaged

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
 import {defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(),],
+    plugins: [react(), tailwindcss(), dts({
+        insertTypesEntry: true,
+        tsconfigPath: './tsconfig.build.json',
+    }),],
     test: {
         environment: "jsdom",
         globals: true,
     },
+    build: {
+        lib: {
+            entry: 'src/index.ts',
+            name: 'FlashcardGames',
+            fileName: 'flashcard-games',
+            formats: ['es', 'cjs']
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'], // don't include in final bundle
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM'
+                }
+            }
+        }
+    }
 })
