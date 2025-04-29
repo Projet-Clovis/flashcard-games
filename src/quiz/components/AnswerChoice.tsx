3629feae8036a72aa1b70c980e6235048e45d8fa
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-30 01:48:00 +0200

    feat: add shaking animation for wrong answers

--- src/quiz/components/AnswerChoice.tsx
+++ src/quiz/components/AnswerChoice.tsx
@@ -1,12 +1,23 @@
+import { motion } from "framer-motion";
+
 export interface AnswerChoiceProps {
     option: string;
     selectedAnswer: string | null;
     isAnswerCorrect: boolean | null;
     onAnswerSelect: (answer: string) => void;
-} // todo: should be renamed
+} // TODO: should be renamed
 
 const correctAnswerStyle = "bg-green-100 text-green-900 border-green-700";
 const wrongAnswerStyle = "bg-red-100 text-red-900 border-red-700";
+
+const shakeAnimation = {
+    x: [0, -2, 2, -2, 2, 0],
+    rotate: [0, -2, 2, -2, 2, 0],
+    // backgroundColor: ["#fff", "#EB5757", "#fff"],
+    transition: {
+        duration: 0.5,
+    },
+};
 
 export const AnswerChoice = ({
     option,
@@ -14,22 +25,24 @@ export const AnswerChoice = ({
     isAnswerCorrect,
     onAnswerSelect,
 }: AnswerChoiceProps) => {
+    const isSelected = selectedAnswer === option;
+    const isWrong = isSelected && isAnswerCorrect === false;
+
+    const selectedClass = isSelected
+        ? isAnswerCorrect
+            ? correctAnswerStyle
+            : wrongAnswerStyle
+        : "bg-gray-100 hover:bg-gray-200";
+
     return (
-        <button
+        <motion.button
             type="button"
-            onClick={() => {
-                onAnswerSelect(option);
-            }}
-            disabled={Boolean(selectedAnswer)} // Désactive après réponse
-            className={`w-full p-3 rounded-lg border border-gray-300 ${
-                selectedAnswer === option
-                    ? isAnswerCorrect
-                        ? correctAnswerStyle
-                        : wrongAnswerStyle
-                    : "bg-gray-100 hover:bg-gray-200"
-            }`}
+            onClick={() => onAnswerSelect(option)}
+            disabled={Boolean(selectedAnswer)}
+            className={`w-full p-3 rounded-lg border border-gray-300 ${selectedClass}`}
+            animate={isWrong ? shakeAnimation : {}}
         >
             {option}
-        </button>
+        </motion.button>
     );
 };
