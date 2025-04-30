1d0127d2a21dfb64179d1d390c8e6693e56777b0
Ab2nour <Ab2nour@users.noreply.github.com>
2025-04-30 00:33:50 +0000

    deploy: af5ba6421a69cd592fb3dfef327b54ab53d90090

---
+++ assets/js/44531ecb.9e01c964.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[7886],{4877:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/flashcard-games/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
