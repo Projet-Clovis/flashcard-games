65528d951989433fa90f026417cc786aa8884cec
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2025-04-30 02:02:46 +0200

    feat(ui): add colored timer bar

--- src/shared/components/TimerBar.tsx
+++ src/shared/components/TimerBar.tsx
@@ -3,14 +3,36 @@ interface TimerBarProps {
     timeLimit: number;
 }
 
+function getColor(percentage: number): string {
+    let barColor = "bg-green-600";
+
+    if (percentage < 100) {
+        barColor = "bg-green-600";
+    }
+    if (percentage < 75) {
+        barColor = "bg-yellow-600";
+    }
+    if (percentage < 50) {
+        barColor = "bg-orange-600";
+    }
+    if (percentage < 25) {
+        barColor = "bg-red-600";
+    }
+
+    return barColor;
+}
+
 export const TimerBar = ({ timeLeft, timeLimit }: TimerBarProps) => {
+    const percentage = (timeLeft / timeLimit) * 100;
+    const barColor = getColor(percentage);
+
     return (
         <div className="w-full mb-4">
             <div className="h-2 bg-gray-200 rounded-full">
                 <div
-                    className="h-2 bg-blue-500 rounded-full transition-all duration-1000 ease-in-out"
+                    className={`${barColor} h-2 rounded-full transition-all duration-1000 ease-in-out`}
                     style={{
-                        width: `${String((timeLeft / timeLimit) * 100)}%`,
+                        width: `${percentage}%`,
                     }}
                 />
             </div>
